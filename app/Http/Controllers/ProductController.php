229e0af62b74eaeaa8de19cfd05e8b4c0a7dc144
Glenn Rudge <glennrudge@outlook.com>
2023-01-19 15:39:58 -0500

    Create lorem ipsum

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,7 +47,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return "Show Product";
+        return "Show Product Lorem ipsum";
     }
 
     /**
