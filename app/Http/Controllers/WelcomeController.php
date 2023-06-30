c29559d2f62be8af7df6f37639b22418e8c3747b
Glenn Rudge <glenn.rudge@outlook.com>
2023-06-30 01:59:16 -0400

    Update AppServiceProvider.php, to use Bootstrap for pagination.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,9 +12,7 @@ class WelcomeController extends Controller
 {
     public function index(Request $request)
     {
-//        $posts = Post::published()->orderBy('created_at', 'DESC')->get();
-        $posts = Post::published()->orderBy('created_at', 'DESC')->paginate(5);
-//        $posts = DB::table("posts")->paginate(5);
+        $posts = Post::published()->orderBy('created_at', 'DESC')->paginate(4);
 
         $clientSearch = $request->input('client-search');
 
