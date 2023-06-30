1bd7dc8d3bfa7ce3db1915c73a458a64c0506f93
Glenn Rudge <glenn.rudge@outlook.com>
2023-06-30 01:13:26 -0400

    Update blog.blade.php

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -18,12 +18,9 @@
 </head>
 
 <body>
-
-
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light navbar-stick-dark" data-navbar="sticky">
     <div class="container">
-
         {{--        <div class="navbar-left">--}}
         {{--            <button class="navbar-toggler" type="button">&#9776;</button>--}}
         {{--            <a class="navbar-brand" href="../index.html">--}}
@@ -31,10 +28,8 @@
         {{--                <img class="logo-light" src="../assets/img/logo-light.png" alt="logo">--}}
         {{--            </a>--}}
         {{--        </div>--}}
-
         <section class="navbar-mobile">
             <span class="navbar-divider d-mobile-none"></span>
-
             <ul class="nav nav-navbar">
                 <li class="nav-item">
                     <a class="nav-link" href="/">HyperAppLabs</a>
