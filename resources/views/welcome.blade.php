70b1927b2768ee9857bece89cbb135bc8e143d04
Glenn Rudge <glenn.rudge@outlook.com>
2023-06-30 02:07:15 -0400

    Update AppServiceProvider.php, to use Bootstrap for pagination.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,6 +40,11 @@
                                 @foreach ($posts as $post)
                                     <div class="col-md-6">
                                         <div class="card border hover-shadow-6 mb-6 d-block">
+                                            <div>
+                                                <a href="" class="badge badge-pill badge-success">
+                                                    Premium
+                                                </a>
+                                            </div>
                                             <img class="card-img-top" src="{{ secure_asset("storage/{$post->image}") }}"
                                                  alt="post image cap">
                                             <div class="p-6 text-center">
