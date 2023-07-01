1984df2095dbecbab544c422c9e5ff716540b05b
Glenn Rudge <glenn.rudge@outlook.com>
2023-06-30 22:15:56 -0400

    Update blog.blade.php and show.blade.php, include video player in show.blade.php.

--- app/Http/Requests/Posts/StorePostRequest.php
+++ app/Http/Requests/Posts/StorePostRequest.php
@@ -26,7 +26,7 @@ class StorePostRequest extends FormRequest
             'description' => 'required|string|min:10',
             'content' => 'required|string|min:10',
             'image' => 'required|image',
-            'video' => 'mimetypes:video/x-matroska,video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi',
+            'video' => 'mimetypes:video/webm,video/x-matroska,video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi',
             'published_at' => 'required|date',
             'category' => 'required',
             'tags' => 'nullable',
