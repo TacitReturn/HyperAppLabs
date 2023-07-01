90dcf6331474ecfe06043f66e8cfa38dd256d74a
Glenn Rudge <glenn.rudge@outlook.com>
2023-06-30 20:08:17 -0400

    Update StorePostRequest.php, included video requirements.

--- app/Http/Requests/Posts/StorePostRequest.php
+++ app/Http/Requests/Posts/StorePostRequest.php
@@ -26,6 +26,7 @@ class StorePostRequest extends FormRequest
             'description' => 'required|string|min:10',
             'content' => 'required|string|min:10',
             'image' => 'required|image',
+            'video' => 'mimetypes:video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi',
             'published_at' => 'required|date',
             'category' => 'required',
             'tags' => 'nullable',
