7c5cd7ef9efd95833f085ccf859bdbd7b1bf4564
Glenn Rudge <glenn.rudge@outlook.com>
2023-07-02 03:19:44 -0400

    Create created.blade.php in mail/posts directory.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -72,10 +72,12 @@ class Post extends Model
         return $query->where('published_at', '<=', now());
     }
 
-//    public static function boot()
-//    {
-//        static::created(function () {
-//            dd('Post Created');
-//        });
-//    }
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function () {
+            dd("Post created");
+        });
+    }
 }
