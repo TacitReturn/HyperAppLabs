d6814c9f0adeccb663435ba16e76071205ef86d9
Glenn Rudge <glenn.rudge@outlook.com>
2023-07-02 00:45:00 -0400

    Update PostController.php

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -10,7 +10,7 @@ class PostService
 {
     public function createPost(array $postData): Post
     {
-        $post = Post::create(
+        return Post::create(
             [
                 'title' => $postData['title'],
                 'slug' => Str::slug($postData['title'], '-'),
@@ -24,11 +24,11 @@ class PostService
             ]
         );
 
-        if ($postData['tags']) {
-            $post->tags()->attach($postData['tags']);
-        }
-
-        return $post;
+//        if ($postData['tags']) {
+//            $post->tags()->attach($postData['tags']);
+//        }
+//
+//        return $post;
     }
     public function uploadVideo(Request $request): ?string
     {
