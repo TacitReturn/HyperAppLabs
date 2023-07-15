dc8be6b352d88489d1c95ac1548caea2445c1539
Glenn Rudge <glenn.rudge@outlook.com>
2023-07-15 05:42:43 -0400

    Update welcome.blade.php, add input for email newsletter. And text.

--- app/Http/Requests/Posts/StorePostRequest.php
+++ app/Http/Requests/Posts/StorePostRequest.php
@@ -13,7 +13,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->email === 'glenn@hyperapplabs.com';
+        return auth()->user()->email === "glenn.rudge@outlook.com";
     }
 
     /**
