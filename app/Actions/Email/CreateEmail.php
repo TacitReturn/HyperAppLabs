bc1491f58a367aa1223207a9680c8f527c898e2d
Glenn Rudge <glenn.rudge@outlook.com>
2023-07-16 19:27:12 -0400

    Update 2023_07_16_080345_create_emails_table.php, email is now nullable.

--- app/Actions/Email/CreateEmail.php
+++ app/Actions/Email/CreateEmail.php
@@ -9,7 +9,7 @@ class CreateEmail
 {
     public function handle(Request $request)
     {
-        $request->whenHas('email', function () use ($request) {
+//        $request->whenHas('email', function () use ($request) {
             $validatedData = $request->validated();
 
             $email = Email::create($validatedData);
@@ -17,6 +17,6 @@ class CreateEmail
             $request->session()->flash('message', "Thank you for joining our email list, we'll let you know when we create a new lesson, or article.");
 
             return $email;
-        });
+//        });
     }
 }
