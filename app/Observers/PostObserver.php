3668accab95d093ac378469cbecb262c5b66f905
TacitReturn <TacitReturn@users.noreply.github.com>
2023-07-17 00:16:30 +0000

    Dusting

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -3,11 +3,11 @@
 namespace App\Observers;
 
 use App\Mail\PostCreated;
+use App\Models\Email;
 use App\Models\Post;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use App\Models\Email;
-use \Illuminate\Support\Collection;
 
 class PostObserver
 {
@@ -18,19 +18,18 @@ class PostObserver
      */
     public function created(Post $post)
     {
-//        $emails = DB::table("emails")->where("email", "!=", null)
-//            ->chunkById(100, function (Collection $emails) use ($post) {
-//                foreach ($emails as $email) {
-//                    Mail::to($email->email)->send(new PostCreated($post));
-//                }
-//            });
+        //        $emails = DB::table("emails")->where("email", "!=", null)
+        //            ->chunkById(100, function (Collection $emails) use ($post) {
+        //                foreach ($emails as $email) {
+        //                    Mail::to($email->email)->send(new PostCreated($post));
+        //                }
+        //            });
 
-        $emails = DB::table("emails")->get();
+        $emails = DB::table('emails')->get();
 
-        $emails = $emails->pluck("email");
+        $emails = $emails->pluck('email');
 
-        foreach($emails as $email)
-        {
+        foreach ($emails as $email) {
             Mail::to($email)->send(new PostCreated($post));
         }
 
@@ -39,10 +38,10 @@ class PostObserver
          * Return the appropriate message.
          */
         if (Mail::failures() != 0) {
-            info("Emails have been sent successfully.");
+            info('Emails have been sent successfully.');
         }
 
-        info("Oops! There was some error sending the emails.");
+        info('Oops! There was some error sending the emails.');
     }
 
     /**
