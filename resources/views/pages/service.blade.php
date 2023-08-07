87787d99bb00121fa06b01d7e7c0351165c8afbd
TacitReturn <glennrudge@outlook.com>
2023-08-07 14:37:06 -0400

    Update service.blade.php

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -32,7 +32,7 @@
             </div>
 
             <div class="col-md-5 mx-auto">
-                <i class="fa-brands fa-laravel"></i>
+                <i   class="display-5" class="fa-brands fa-laravel"></i>
               <img class="rounded-md" src="../assets/img/thumb/5.jpg" alt="...">
             </div>
 
