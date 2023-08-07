42d33c4f9e176386bfc154b008140f356c88e89d
TacitReturn <glennrudge@outlook.com>
2023-08-07 16:37:37 -0400

    Update blog.blade.php

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,4 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
-class ReactForm extends Component {
+class ReactForm extends React.Component {
     render() {
         return (
             <form
