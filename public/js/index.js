65de7259622c380fca9f5d93904b15b7b77b23d0
TacitReturn <glennrudge@outlook.com>
2023-08-07 18:00:05 -0400

    Update blog.blade.php

--- public/js/index.js
+++ public/js/index.js
@@ -81,7 +81,7 @@ class ReactForm extends React.Component {
                         onChange={this.handleChange}
                         class="form-control form-control-lg"
                         name="budget"
-                        value={selectOption}
+                        value={selectValue}
                     >
                         <option value="1">Up to $1,000</option>
                         <option value="2">Up to $3,000</option>
