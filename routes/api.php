d737252882490e959bd888d23ac94e1140767a74
TacitReturn <TacitReturn@users.noreply.github.com>
2023-08-09 21:32:19 +0000

    Dusting

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::post('contact', function (Request $request) {
     return response()->json(['message' => $request->all()]);
-})->middleware("auth");
+})->middleware('auth');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
