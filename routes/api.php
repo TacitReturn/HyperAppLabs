0cd779ee8dda19718a3ede3d033175a607a0c463
TacitReturn <glennrudge@outlook.com>
2023-08-09 17:32:46 -0400

    Update welcome.lade.php

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('contact', function (Request $request) {
     return response()->json(['message' => $request->all()]);
-})->middleware("auth");
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
