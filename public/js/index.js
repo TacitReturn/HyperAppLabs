1ed8a780e39dd2b302ecfee5b0a4d43347f3933e
TacitReturn <glennrudge@outlook.com>
2023-08-10 18:47:11 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -50,7 +50,7 @@ class ReactForm extends React.Component {
             .then((response) => response.json())
             .then((json) => console.log(json))
             .catch((error) => {
-                console.log(JSON.stringify(error.message));
+                console.log(error);
             });
     }
 
