ab507c46d656a883d1ca16b551311328eddaeac7
TacitReturn <glennrudge@outlook.com>
2023-08-10 14:12:25 -0400

    Create ContactForm Request, update request/api.php and update ContactForm to ContactFormRequest

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\ContactFormRequest;
 
-Route::post('contact', function (Request $request) {
+Route::post('contact', function (ContactFormRequest $request) {
     $user = User::find(1);
 
     $validatedData = $request->validated();
@@ -15,7 +16,7 @@ Route::post('contact', function (Request $request) {
 
     Mail::to($user->email)->send(new Contact($contactForm));
 
-    return response()->json(["success" => "Message sent successfully. We will reach back out to you as soon as possible aobut your inquiry."]);
+    return response()->json(["success" => "Thank you for your interest in doing busines {$contactForm->name}. We will reach back out to you as soon as possible aobut your inquiry."]);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
