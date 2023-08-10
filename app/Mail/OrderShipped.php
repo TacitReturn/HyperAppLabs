3d94a1ff80c513ab3662d854609ec2730a79d74b
TacitReturn <TacitReturn@users.noreply.github.com>
2023-08-10 19:16:05 +0000

    Dusting

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
@@ -11,11 +10,6 @@ class OrderShipped extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         //
--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 # Business Inuiry
-## This business inquiry is from {{ $contactForm->name}}
+## This business inquiry is from {{ $contactForm->name }}
 <p>Email: {{ $companyForm->email }}</p> <br />
 <p>Company {{ $companyForm->company }}</p> <br />
 <p>Budget: {{ $companyForm->budget }}</p> <br />
