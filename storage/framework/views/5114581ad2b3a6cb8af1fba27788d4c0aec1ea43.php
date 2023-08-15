4f134537942f57f8f4b57607e30e32c71d4ea5a5
TacitReturn <glennrudge@outlook.com>
2023-08-15 16:55:20 -0400

    Update login view

--- storage/framework/views/5114581ad2b3a6cb8af1fba27788d4c0aec1ea43.php
+++
@@ -1,9 +0,0 @@
-<?php if(session()->has("status")): ?>
-    <div class="alert alert-success">
-        <p class="text-center">
-            <?php echo e(session()->get("status")); ?>
-
-        </p>
-    </div>
-<?php endif; ?>
-<?php /**PATH /home/forge/hyperapplabs.com/resources/views/partials/success.blade.php ENDPATH**/ ?>
--- storage/framework/views/d36ca199a8a26d798f3e054b453a067b09a1e67c.php
+++
@@ -1,9 +0,0 @@
-<?php if(session()->has("error")): ?>
-    <div class="alert alert-danger">
-        <p class="text-center">
-            <?php echo e(session()->get("error")); ?>
-
-        </p>
-    </div>
-<?php endif; ?>
-<?php /**PATH /home/forge/hyperapplabs.com/resources/views/partials/errors.blade.php ENDPATH**/ ?>
