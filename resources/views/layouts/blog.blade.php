f2bb0c083229af888500dcc6e9b25088e1950424
Glenn <glennrudge@outlook.com>
2023-08-15 23:01:00 -0400

    Update blog.blade.php

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -99,21 +99,6 @@
 <footer class="footer">
     <div class="container">
         <div class="row gap-y align-items-center">
-
-            {{--            <div class="col-6 col-lg-3">--}}
-            {{--                <a href="../index.html"><img src="../assets/img/logo-dark.png" alt="logo"></a>--}}
-            {{--            </div>--}}
-
-            <div class="col-6 col-lg-3 text-right order-lg-last">
-                {{--                <div class="social">--}}
-                {{--                    <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>--}}
-                {{--                    <a class="social-twitter" href="https://twitter.com/thethemeio"><i class="fa fa-twitter"></i></a>--}}
-                {{--                    <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i--}}
-                {{--                                class="fa fa-instagram"></i></a>--}}
-                {{--                    <a class="social-dribbble" href="https://dribbble.com/thethemeio"><i class="fa fa-dribbble"></i></a>--}}
-                {{--                </div>--}}
-            </div>
-
             <div class="col-lg-6">
                 <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
                     <a target="_blank" class="nav-link" href="https://www.linkedin.com/in/glenn-rudge/">
